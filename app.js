b75d0b43514f47dcd9aeca378bd8659b3e2066f6
primasghar <mirpasghar@gmail.com>
2024-09-13 14:31:41 +0300

    Added view template and Dockerfile

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 import { serve } from "./deps.js";
+import { configure, renderFile } from "./deps.js";
+
+
+configure({
+    views: `${Deno.cwd()}/views/`,
+  });
+
+  const responseDetails = {
+    headers: { "Content-Type": "text/html;charset=UTF-8" },
+  };    
+
+  const data = {
+    count: 0,
+  };
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+    const url = new URL(request.url);
+    if (url.pathname === "/count") {
+      data.count++;
+      return new Response(await renderFile("count.eta", data), responseDetails);
 };
+};
+
+return new Response("Hello you!");
 
 serve(handleRequest, { port: 7777 });
